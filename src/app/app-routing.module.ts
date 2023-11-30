9156851e9a4feef5a0a3999d930bb244b56504b6
dhruvin-mehta-comprinno <dhruvin.mehta@comprinno.net>
2023-11-30 13:46:34 +0530

    code commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-us', pathMatch: 'full', component: AboutUsComponent},
+  { path: '', pathMatch: 'full', component: HomeComponent},
+  { path: 'dashboard', pathMatch: 'full', component: DashboardComponent},
+  { path: 'products', pathMatch: 'full', component: ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
